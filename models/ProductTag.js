8ce62b1d6a0cdd9403717da43690bc2e0e75e30a
Michael Astalos <michaeldont@gmail.com>
2021-09-18 17:32:58 -0400

    updated models with requirements

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,23 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
