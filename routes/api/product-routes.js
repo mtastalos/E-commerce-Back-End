b3d97a461d75d51f99dda7e302fcdbca8e919793
Michael Astalos <michaeldont@gmail.com>
2021-09-19 20:00:21 -0400

    finished product routes, moving to tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {model: Category},
+      {model: Tag}
+    ]
+  })
+  .then(results => res.json(results))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {id: req.params.id},
+    include: [
+      {model: Category},
+      {model: Tag}
+    ]
+  })
+  .then(results => res.json(results))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -91,6 +114,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+    // delete a category by its `id` value
+    Product.destroy({
+      where: {id: req.params.id}
+    })
+    .then(results => res.json(results))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
